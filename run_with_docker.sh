151a34ce87a509914bb261fa4168b24ea269058e
Christopher Obiozor <cobiozor@andrew.cmu.edu>
2026-05-22 10:57:13 -0400

    revise docker run script to expect to be passed a spark run file, and add commnets

--- run_with_docker.sh
+++ run_with_docker.sh
@@ -2,16 +2,29 @@
 
 set -euo pipefail
 
+usage (){
+	echo "Usage: $0 {ros|wsl|default} <spark-run-file>" >&2
+}
+
 profile="${1:-default}"
 
+# check for container variant
 case "$profile" in
 	ros|wsl|default)
 		shift || true
 		;;
 	*)
-		echo "Usage: $0 {ros|wsl|default} [docker-compose-args...]" >&2
+		usage
+		echo 'Must specify a container variant, e.g., "default"'
 		exit 1
 		;;
 esac
 
-UID_UID=$(id -u):$(id -g) docker compose run spark-${profile} "$@" 
+# check for run file
+if [[ $# -eq 0 ]]; then
+	usage
+	echo "Must specify a spark run file"
+	exit 1
+fi
+
+docker compose run --remove-orphans spark-${profile} bash -ic "source /root/.bashrc && python $1"
